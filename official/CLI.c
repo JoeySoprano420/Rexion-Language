9315718e9f483c664f4a7cf4fa5102ded5d8b193
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-06-11 13:06:36 -0400

    Update CLI.c

--- official/CLI.c
+++ official/CLI.c
@@ -114,3 +114,130 @@ int main(int argc, char** argv) {
 
     return 0;
 }
+
+// parser.c – Rexion Parser (Simplified)
+#include "parser.h"
+#include <stdio.h>
+
+int current = 0;
+
+Token peek() {
+    return tokens[current];
+}
+
+Token advance() {
+    return tokens[current++];
+}
+
+void match(TokenType type) {
+    if (peek().type == type) advance();
+    else {
+        printf("Syntax Error: Expected token type %d\n", type);
+        exit(1);
+    }
+}
+
+void parse_program() {
+    while (peek().type != TOKEN_EOF) {
+        parse_statement();
+    }
+}
+
+void parse_statement() {
+    Token t = peek();
+    if (t.type == TOKEN_DEFINE) {
+        parse_define();
+    } else if (t.type == TOKEN_FUNC) {
+        parse_func();
+    } else if (t.type == TOKEN_PRINT) {
+        parse_print();
+    } else if (t.type == TOKEN_CLASS) {
+        parse_class();
+    } else if (t.type == TOKEN_PUBLIC || t.type == TOKEN_PRIVATE || t.type == TOKEN_PROTECTED) {
+        parse_visibility();
+    } else if (t.type == TOKEN_NEW) {
+        parse_new();
+    } else if (t.type == TOKEN_SUPER) {
+        parse_super();
+    } else if (t.type == TOKEN_THIS) {
+        parse_this();
+    } else if (t.type == TOKEN_EVAL) {
+        parse_eval();
+    }
+    // Graphics & Audio Advanced Features
+    else if (
+        t.type == TOKEN_RAYTRACING ||
+        t.type == TOKEN_VECTORIZE ||
+        t.type == TOKEN_SHADING ||
+        t.type == TOKEN_TUNING ||
+        t.type == TOKEN_TRACKING ||
+        t.type == TOKEN_TRACING ||
+        t.type == TOKEN_RENDERING ||
+        t.type == TOKEN_STACKING ||
+        t.type == TOKEN_LAYERING ||
+        t.type == TOKEN_DIALING ||
+        t.type == TOKEN_PARTICLE_PHYSICS ||
+        t.type == TOKEN_MODDING ||
+        t.type == TOKEN_TEXTURING ||
+        t.type == TOKEN_SCULPTING ||
+        t.type == TOKEN_MOLDING ||
+        t.type == TOKEN_RIGGING ||
+        t.type == TOKEN_FLESH ||
+        t.type == TOKEN_BONES ||
+        t.type == TOKEN_SMOKE ||
+        t.type == TOKEN_STREAMING ||
+        t.type == TOKEN_STEAM ||
+        t.type == TOKEN_DIRECTIONALS ||
+        t.type == TOKEN_DURATIONS ||
+        t.type == TOKEN_3D ||
+        t.type == TOKEN_SCANNING ||
+        t.type == TOKEN_LIGHTING ||
+        t.type == TOKEN_TRANSITIONS ||
+        t.type == TOKEN_MOTION ||
+        t.type == TOKEN_AGING ||
+        t.type == TOKEN_WITHERING ||
+        t.type == TOKEN_MOTION_DETECTION ||
+        t.type == TOKEN_COLLISION_DETECTION ||
+        t.type == TOKEN_MORPHING ||
+        t.type == TOKEN_RANGE ||
+        t.type == TOKEN_SCOPE ||
+        t.type == TOKEN_SCALING ||
+        t.type == TOKEN_TELESCOPICS ||
+        t.type == TOKEN_OPTICS ||
+        t.type == TOKEN_INTRINSIC_MEASURING ||
+        t.type == TOKEN_MATRIX ||
+        t.type == TOKEN_LASERING ||
+        t.type == TOKEN_ZOOM ||
+        t.type == TOKEN_PINCH ||
+        t.type == TOKEN_CARD_CREATION ||
+        t.type == TOKEN_VOICE ||
+        t.type == TOKEN_MUSIC ||
+        t.type == TOKEN_CROSS_VECTOR ||
+        t.type == TOKEN_FRAMING ||
+        t.type == TOKEN_CAD ||
+        t.type == TOKEN_BLUEPRINTING ||
+        t.type == TOKEN_CYCLING ||
+        t.type == TOKEN_FOOTPRINTS ||
+        t.type == TOKEN_IMPRESSIONS ||
+        t.type == TOKEN_FINGERPRINTING ||
+        t.type == TOKEN_BOXING ||
+        t.type == TOKEN_UNBOXING ||
+        t.type == TOKEN_BACKGROUND_INTERACTIVITY ||
+        t.type == TOKEN_WIND ||
+        t.type == TOKEN_PIPELINES ||
+        t.type == TOKEN_DETAILING ||
+        t.type == TOKEN_FENCING ||
+        t.type == TOKEN_WORLD_BUILDING ||
+        t.type == TOKEN_WIPING ||
+        t.type == TOKEN_COLOR ||
+        t.type == TOKEN_VISION ||
+        t.type == TOKEN_COMPUTER_VISION ||
+        t.type == TOKEN_CONVERSIONS ||
+        t.type == TOKEN_FIXING ||
+        t.type == TOKEN_UNFIXING ||
+        t.type == TOKEN_CUTTING ||
+        t.type == TOKEN_SECTIONING ||
+        t.type == TOKEN_PARTITIONING ||
+        t.type == TOKEN_CIPHERING ||
+        t.type == TOKEN_ENCRYPTION ||
+        t.type == TOKEN_SHADING
